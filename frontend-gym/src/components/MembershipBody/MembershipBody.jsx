7a6ffc5c9843136609cc1b2c5a53cddd61ab03b1
ksahana <k_sahana@trimble.com>
2025-03-15 00:25:37 +0530

    modal ui added

--- frontend-gym/src/components/MembershipBody/MembershipBody.jsx
+++ frontend-gym/src/components/MembershipBody/MembershipBody.jsx
@@ -1,9 +1,65 @@
 import { CiCirclePlus } from "react-icons/ci";
 import { useEffect, useState } from "react";
 import MembershipsTable from "../MembershipsTable/MembershipsTable";
+import MembershipModal from "../MembershipModal/MembershipModal";
 
 
 const MembershipBody = () => {
+    const [isDisplay, setIsDisplay] = useState(false)
+    const openModal = () => {
+        setIsDisplay(true)
+        console.log('opened modal')
+    }
+    const closeModal = () => {
+        setIsDisplay(false)
+    }
+    const [customers, setCustomers] = useState([
+        {
+            name: "Rajesh Wagle",
+            email: "rajesh@gmail.com",
+            role: "member",
+            age: 23,
+            phoneNo: "0932840832",
+            branch: "Gujarat",
+            status: "expired",
+            password: "randomPassword",
+            activeSubscriptionId: "219f47d2-905d-42ae-b920-b5dd53959752",
+            id: "5c007bf5-cb8f-4dca-8d5f-89642eb99e93"
+        }, {
+            name: "Harsha Javvaji",
+            email: "harsha@gmail.com",
+            role: "member",
+            age: 23,
+            phoneNo: "0932840832",
+            branch: "Gujarat",
+            status: "active",
+            password: "randomPassword",
+            activeSubscriptionId: "46f1e197-0755-474f-93d8-1096f5eeb138",
+            upcomingSubscriptionId: "46f1e197-0755-474f-93d8-1096f5eeb138",
+            id: "219f47d2-905d-42ae-b920-b5dd53959752"
+        }, {
+            name: "Chaitanya",
+            email: "chaitu@gmail.com",
+            role: "member",
+            age: 23,
+            phoneNo: "0932840832",
+            branch: "Hyderabad",
+            status: "newUser",
+            password: "randomPassword",
+            id: "219f47d2-905d-42ae-b920-b5dd53959752"
+        }, {
+            name: "Sahana",
+            email: "sahana@gmail.com",
+            role: "member",
+            age: 23,
+            phoneNo: "0932840832",
+            branch: "Mumbai",
+            status: "active",
+            password: "randomPassword",
+            activeSubscriptionId: "5c007bf5-cb8f-4dca-8d5f-89642eb99e93",
+            id: "219f47d2-905d-42ae-b920-b5dd53959752"
+        }
+    ])
     const [memberships, setMemberships] = useState([{
         name: "Rajesh Wagle",
         type: "Super",
@@ -34,6 +90,49 @@ const MembershipBody = () => {
         status: "epired"
     }
     ])
+    
+    const [subscriptions, setSubscriptions] = useState([
+        {
+            amount: "5000",
+            isActive: true,
+            id: "c89b7df9-949c-410c-85bf-f0a55b2077cf",
+            name: "Basic",
+            validity: "3m",
+            type: "Subscription"
+        },
+        {
+            amount: "7000",
+            isActive: true,
+            id: "c89b7df9-949c-410c-85bf-f0a55b2077cf",
+            name: "Super",
+            validity: "5m",
+            type: "Subscription"
+        },
+        {
+            amount: "10000",
+            isActive: true,
+            id: "c89b7df9-949c-410c-85bf-f0a55b2077cf",
+            name: "Silver",
+            validity: "7m",
+            type: "Subscription"
+        },
+        {
+            amount: "15000",
+            isActive: true,
+            id: "c89b7df9-949c-410c-85bf-f0a55b2077cf",
+            name: "Gold",
+            validity: "18m",
+            type: "Subscription"
+        },
+        {
+            amount: "17000",
+            isActive: true,
+            id: "c89b7df9-949c-410c-85bf-f0a55b2077cf",
+            name: "Premium",
+            validity: "24m",
+            type: "Subscription"
+        },
+    ])
 
     return (<div className="container-fluid" >
         <div className="d-flex justify-content-between align-items-center" >
@@ -41,13 +140,14 @@ const MembershipBody = () => {
                 <h1>Memberships</h1>
                 <p>Manage your gym memberships</p>
             </div>
-            <button type="button" className="btn btn-secondary d-flex justify-content-between align-items-center gap-2"  ><CiCirclePlus /> <span>Add Membership</span> </button>
+            <button type="button" onClick={openModal} className="btn btn-secondary d-flex justify-content-between align-items-center gap-2"  ><CiCirclePlus /> <span>Add Membership</span> </button>
         </div>
         <form className="d-flex container" role="search">
             <input className="form-control me-2" type="search" placeholder="Search memberships..." aria-label="Search" />
             <button className="btn btn-outline-secondary" type="submit">Search</button>
         </form>
         <MembershipsTable memberships={memberships} />
+        {isDisplay && <MembershipModal customers={customers} subscriptions={subscriptions} closeModal={closeModal} />}
     </div>)
 }
 
---
+++ frontend-gym/src/components/MembershipModal/MembershipModal.jsx
@@ -0,0 +1,49 @@
+import "./membershipmodal.css"
+import { IoMdClose } from "react-icons/io";
+
+const MembershipModal = ({ closeModal, customers, subscriptions }) => {
+    // centered-content is className for centering the content (like a modal)
+    return (
+        <div className="centered-content d-flex flex-column">
+            <div className="d-flex">
+                <div className="d-flex flex-column">
+                    <h5>Add New Membership</h5>
+                    <span className="text-secondary" >Enter the details for the new membership.</span>
+                </div>
+                <IoMdClose onClick={closeModal} />
+            </div>
+
+            <div className="mb-3 mt-2">
+                <label htmlFor="custList" className="form-label">Customer</label>
+                <select id="custList" className="form-select">
+                    <option value="" selected>Select a customer</option>
+                    {
+                        customers.map((customer, index) => {
+                            return <option key={customer.id} value={customer.id}>{customer.name} </option>
+                        })
+                    }
+                </select>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="subsList" className="form-label">Membership Type</label>
+                <select id="subsList" className="form-select">
+                    <option value="" selected>Select a subscription</option>
+                    {
+                        subscriptions.map((subs, ind) => {
+                            return <option key={subs.id} value={subs.id}>{subs.name}-{subs.validity} </option>
+                        })
+                    }
+                </select>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="startDateId" className="form-label">Start Date</label>
+                <input id="startDateId" className="form-control" type="datetime-local" />
+            </div>
+            <div className="d-flex justify-content-end">
+                <button onClick={closeModal} className="btn btn-secondary">Add Membership</button>
+            </div>
+        </div>
+    )
+}
+
+export default MembershipModal;
