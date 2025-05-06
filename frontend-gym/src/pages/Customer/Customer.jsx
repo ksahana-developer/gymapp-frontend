558d3d8de93c053e270b2597941afa19d1970cbb
ksahana <k_sahana@trimble.com>
2025-05-06 23:59:28 +0530

    small fix about the myActivity & editModal button

--- frontend-gym/src/pages/Customer/Customer.jsx
+++ frontend-gym/src/pages/Customer/Customer.jsx
@@ -21,8 +21,8 @@ const Customer = () => {
     const [isDisplayEdit, setIsDisplayEdit] = useState(false)
     const [fetchCustomer, setFetchCustomer] = useState(false)
     const openEditModal = () => {
-        if(!isDisplayEdit)
-        setIsDisplayEdit(true)
+        if (!isDisplayEdit)
+            setIsDisplayEdit(true)
     }
 
     const closeEditModal = () => {
@@ -80,21 +80,21 @@ const Customer = () => {
     }, [])
 
     useEffect(() => {
-        if(fetchCustomer){
+        if (fetchCustomer) {
             getCustomer()
         }
         setFetchCustomer(false)
     }, [fetchCustomer])
 
     const token = localStorage.getItem('token')
-    const decoded = token? jwtDecode(token) : null
+    const decoded = token ? jwtDecode(token) : null
     return (
         <div className="container-fluid py-4 px-3 px-md-4 animate__animated animate__fadeIn">
             <div className="row mb-4">
                 <div className="col-12">
                     <h2 className="mb-4 fw-bold text-primary">Customer Details</h2>
                     <Link to="/customers" className="text-decoration-none">
-                        <button className="btn btn-light mb-4 shadow-sm hover-scale"> 
+                        <button className="btn btn-light mb-4 shadow-sm hover-scale">
                             <MdOutlineKeyboardArrowLeft />Back to Customers
                         </button>
                     </Link>
@@ -106,16 +106,16 @@ const Customer = () => {
                     <div className="customer-profile-card p-4 bg-white rounded-3 shadow-sm hover-card animate__animated animate__fadeIn">
                         <div className="d-flex flex-column flex-md-row align-items-center gap-4">
                             <div className="profile-image-container">
-                                <img 
-                                    src={localStorage.getItem('customer') ? JSON.parse(localStorage.getItem('customer')).profilePicture : '"https://www.wikibiodata.com/wp-content/uploads/2020/12/Sumeet-Raghvan.jpg"'} 
-                                    alt="Profile" 
+                                <img
+                                    src={localStorage.getItem('customer') ? JSON.parse(localStorage.getItem('customer')).profilePicture : '"https://www.wikibiodata.com/wp-content/uploads/2020/12/Sumeet-Raghvan.jpg"'}
+                                    alt="Profile"
                                     className="rounded-circle profile-image hover-scale"
                                     width="120"
                                     height="120" />
                             </div>
                             <div className="customer-info text-center d-flex flex-column align-items-start text-md-start">
                                 <h4 className="mb-2 fw-bold text-primary">
-                                    {!isLoadCust ? customer?.name : 
+                                    {!isLoadCust ? customer?.name :
                                         <p className="placeholder-glow">
                                             <span className="placeholder col-12"></span>
                                         </p>
@@ -123,13 +123,17 @@ const Customer = () => {
                                 </h4>
                                 <div className="d-flex flex-wrap align-items-center gap-3">
                                     <span className="badge bg-warning text-danger d-flex align-items-center info-item">
-                                        <TbPointFilled className="me-1"/>{customer?.status}
+                                        <TbPointFilled className="me-1" />{customer?.status}
                                     </span>
                                     <p className="text-secondary fs-6 mb-0 info-item">
-                                        <span className="fw-semibold">ID:</span> {!isLoadCust ? `GYM-${customer?.id}` : 
+                                        <span className="fw-semibold">ID:</span> {!isLoadCust ? `GYM-${customer?.id}` :
                                             <span className="placeholder col-12"></span>
                                         }
                                     </p>
+                                </div>
+                                <div className="d-flex gap-3 align-items-center">
+                                    {(id === decoded.id) && <button className="btn btn-light" onClick={() => openEditModal(customer)}><FiEdit /> Edit</button>}
+                                    <Link to={`/activity/customer/${JSON.parse(localStorage.getItem('customer'))?.id}`} className="btn btn-primary btn-sm my-2">View my activity</Link>
                                 </div>
                             </div>
                         </div>
@@ -143,28 +147,28 @@ const Customer = () => {
                         <h4 className="fw-bold mb-3">Contact Information</h4>
                         <p className="text-secondary mb-4">Customer's personal contact details</p>
                         <div className="d-flex align-items-center mb-3 info-item">
-                            <MdOutlineEmail className="me-2 text-primary"/>
-                            <span>{!isLoadCust ? customer?.email : 
+                            <MdOutlineEmail className="me-2 text-primary" />
+                            <span>{!isLoadCust ? customer?.email :
                                 <span className="placeholder col-6"></span>
                             }</span>
                         </div>
                         <div className="d-flex align-items-center info-item">
-                            <FiPhone className="me-2 text-primary"/>
-                            <span>{!isLoadCust ? customer?.phoneNo : 
+                            <FiPhone className="me-2 text-primary" />
+                            <span>{!isLoadCust ? customer?.phoneNo :
                                 <span className="placeholder col-4"></span>
                             }</span>
                         </div>
                     </div>
                 </div>
 
-                {customer?.activeSubscriptionId && 
+                {customer?.activeSubscriptionId &&
                     <div className="col-12 col-md-6">
                         <div className="card shadow-sm border-0 rounded-3 p-4 h-100 hover-card">
                             <h4 className="fw-bold mb-3">Membership Details</h4>
                             <p className="text-secondary mb-4">Information about the customer's membership</p>
                             <div className="d-flex justify-content-between align-items-center mb-3">
                                 <p className="text-secondary mb-0">Type</p>
-                                <p className="mb-0 fw-medium">{!isLoadMem ? membership?.type : 
+                                <p className="mb-0 fw-medium">{!isLoadMem ? membership?.type :
                                     <span className="placeholder col-12"></span>
                                 }</p>
                             </div>
@@ -192,11 +196,11 @@ const Customer = () => {
                 </div>
             </div>
 
-            {isDisplayEdit && 
-                <EditProfileModal 
-                    customer={customer} 
-                    closeEditModal={closeEditModal} 
-                    setFetchCustomer={setFetchCustomer} 
+            {isDisplayEdit &&
+                <EditProfileModal
+                    customer={customer}
+                    closeEditModal={closeEditModal}
+                    setFetchCustomer={setFetchCustomer}
                 />
             }
         </div>
